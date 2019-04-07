95f53fe96333302a6cf55f86607b4fcefb57e30b
pa-m <pascal.masschelier@gmail.com>
2019-04-07 10:31:00 +0200

    add mathrand.go

---
+++ mathrand.go
@@ -0,0 +1,31 @@
+package randomkit
+
+import "math/rand"
+
+type RKMathRandSource struct{ RKState *RKState }
+
+// AsMathRandSource return RKState as a source suitable for rand.New
+func (state *RKState) AsMathRandSource() *RKMathRandSource {
+	return &RKMathRandSource{state}
+}
+
+// seed proto for math/rand source
+func (state *RKMathRandSource) Seed(seed int64) {
+	state.RKState.Seed(uint64(seed))
+}
+
+// Int63 for math/rand source
+func (state *RKMathRandSource) Int63() int64 {
+	return state.RKState.Int63()
+}
+
+// Uint64 for math/rand source
+func (state *RKMathRandSource) Uint64() uint64 {
+	return state.RKState.Uint64()
+}
+
+// Clone allow duplicating source state
+func (state *RKMathRandSource) Clone() rand.Source {
+	newSource := &RKMathRandSource{state.RKState.Clone()}
+	return newSource
+}
